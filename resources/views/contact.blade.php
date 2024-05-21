4eb4f76d3916b18fb4b055633d43c7c7711c9482
Jefri Herdi Triyanto <jefripunza@gmail.com>
2024-05-22 03:53:28 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <p>Welcome to Contact Page</p>
+    <table>
+        <tr>
+            <td>Phone Number</td>
+            <td>{{ $info['phone_number'] }}</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>{{ $info['email'] }}</td>
+        </tr>
+        <tr>
+            <td>Instagram</td>
+            <td>{{ $info['instagram'] }}</td>
+        </tr>
+        <tr>
+            <td>Linkedin</td>
+            <td>{{ $info['linkedin'] }}</td>
+        </tr>
+    </table>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', [
+        'title' => 'Home Page',
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About Page',
+        'name' => "Jefri Herdi Triyanto",
+    ]);
+});
+
+// PR...
+
+$posts = [
+    [
+        "post_slug" => "belajar-laravel-dengan-cepat",
+        "title" => "Belajar Laravel dengan Cepat?",
+        "body" => "saya sedang mencoba untuk belajar laravel secepat mungkin agar bisa menembus batas 7.5 JT, demi anak bojo bree",
+        "author" => "Jefri Herdi Triyanto",
+        "author_slug" => "jefri-herdi-triyanto",
+        "created_at" => "14 Januari 1996",
+    ],
+    [
+        "post_slug" => "mampukah-menerima-cobaan-ini",
+        "title" => "Mampukah Menerima Cobaan Ini?",
+        "body" => "ketika saya niat ingsun, insya allah barokah lancar. Amiinnn....",
+        "author" => "Watini",
+        "author_slug" => "watini",
+        "created_at" => "27 Juli 1992",
+    ],
+];
+
+Route::get('/posts', function () use (&$posts) {
+    return view('posts', [
+        'title' => 'Posts Page',
+        'posts' => $posts,
+    ]);
+});
+
+Route::get('/post/{post_slug}', function ($post_slug) use (&$posts) {
+    // $post = collect($posts)->firstWhere('post_slug', $post_slug);
+    $post = Arr::first($posts, function ($post) use ($post_slug) {
+        return $post['post_slug'] == $post_slug;
+    });
+    return view('post', [
+        'title' => $post['title'],
+        'post' => $post,
+    ]);
+});
+
+Route::get('/contact', function () {
+    $info = [
+        "phone_number" => "082214252455",
+        "email" => "jefriherditriyanto@gmail.com",
+        "instagram" => "@jefripunza",
+        "linkedin" => "https://www.linkedin.com/in/jefri-herdi-triyanto-ba76a8106/",
+    ];
+    return view('contact', [
+        'title' => 'Contact Page',
+        'info' => $info,
+    ]);
+});
