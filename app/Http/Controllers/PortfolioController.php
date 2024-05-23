927c1d615cbeba2b378dcd613e756b681174cb74
Jefri Herdi Triyanto <jefripunza@gmail.com>
2024-05-24 00:22:37 +0700

    save

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PortfolioController extends Controller
 {
     public function manage()
     {
+        if (Auth::guest()) {
+            return redirect()->route('login');
+        }
         return view('user.portfolio', [
             'title' => "User Page",
         ]);
